68ac38ceb578c26ce30c518d93e560ae0f8613ba
Meitnerium9 <gronekmarcinn@gmail.com>
2026-03-31 20:56:22 +0200

    Nowe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import iconFavourite from "./assets/ulubione.png";
+import iconLogo from "./assets/logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app-container">
+      <nav className="navbar">
+        <div className="nav-logo">
+          <img src={iconLogo} alt="Logo" />
+        </div>
+
+        <ul className="nav-menu">
+          <li>
+            <a href="#kreator">Kreator</a>
+          </li>
+          <li>
+            <a href="#kolekcje">Kolekcje</a>
+          </li>
+          <li>
+            <a href="#onas">O nas</a>
+          </li>
+        </ul>
+
+        <div className="nav-actions">
+          <button
+            className="nav-icon-btn"
+            onClick={() => console.log("Szukaj")}
+          >
+            <img src={iconFavourite} alt="Szukaj" className="nav-icon-img" />
+          </button>
+          <button className="nav-icon" onClick={() => console.log("Profil")}>
+            👤
+          </button>
+        </div>
+      </nav>
+
+      <main className="main-content">
+        <h1>Wyświetlarka to do</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
